47897a48d5bf4a7ff9dce9e33805b55d1e8208dd
Apollo <super.hero2017@outlook.com>
2018-01-07 00:45:57 -0600

    Main pages and layout

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,10 +13,16 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $user = $this->getUser();
+
+
+       if ($user == null ){
+           return $this->redirectToRoute('fos_user_security_login', array(
+           ));
+
+       }
+        return $this->redirectToRoute('dashboard', array(
+        ));
     }
 
     /**
@@ -25,4 +31,62 @@ class DefaultController extends Controller
     public function feedAction(Request $request)
     {
     }
+
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboardAction(Request $request)
+    {
+
+        $user = $this->getUser();
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'user' => $user
+        ]);
+
+    }
+
+    /**
+     * @Route("/buy", name="buy")
+     */
+    public function buyAction(Request $request)
+    {
+
+        $user = $this->getUser();
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'user' => $user
+        ]);
+
+    }
+
+    /**
+     * @Route("/sell", name="sell")
+     */
+    public function sellAction(Request $request)
+    {
+
+        $user = $this->getUser();
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'user' => $user
+        ]);
+
+    }
+
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function profileAction(Request $request)
+    {
+
+        $user = $this->getUser();
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'user' => $user
+        ]);
+
+    }
+
+
 }
