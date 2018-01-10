ff0e5cb4d812fa86339f50c3b1795ca1ddd3cfa4
Apollo <super.hero2017@outlook.com>
2018-01-10 17:55:01 -0600

    Reset password fixes

---
+++ src/AppBundle/EventListener/ResetPasswordSuccess.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JuanCruz
+ * Date: 25/12/2017
+ * Time: 3:22 PM
+ */
+
+namespace AppBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Listener responsible to change the redirection at the end of the password resetting
+ */
+class ResetPasswordSuccess implements EventSubscriberInterface
+{
+    protected $twig;
+    protected $mailer;
+
+    public function __construct(\Twig_Environment $twig, \Swift_Mailer $mailer)
+    {
+        $this->twig = $twig;
+        $this->mailer = $mailer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::RESETTING_RESET_COMPLETED => 'onResetPassword',
+        );
+    }
+
+    public function onResetPassword(FilterUserResponseEvent $event)
+    {
+
+        $user = $event->getUser();
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Content Arena')
+            ->setFrom('noreply@contentarena.com', "Content Arena Admin")
+            ->setTo($user->getEmail())
+            ->setBody(
+                $this->twig->render(
+                    'Registration/reset_password_success.txt.twig',
+                    array('user' => $user )
+                )
+            )
+        ;
+        $this->mailer->send($message);
+    }
+}
