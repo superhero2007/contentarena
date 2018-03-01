18269067d97ac30e0677dc2c9f6a02f338f7b3ae
Apollo <super.hero2017@outlook.com>
2018-03-01 16:33:01 -0500

    View seller popup fixes

---
+++ src/AppBundle/Resources/public/javascript/content.js
@@ -0,0 +1,13 @@
+/**
+ * Created by JuanCruz on 1/3/2018.
+ */
+
+$(function () {
+
+    $("#view-seller").on("click", function () {
+        $("#seller-dialog").dialog({
+            width: 800
+        });
+    })
+
+});
