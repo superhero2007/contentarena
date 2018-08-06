619827e54b4f84d8fca30e8607a6ba90f04056cb
Apollo <super.hero2017@outlook.com>
2018-08-06 01:37:59 -0300

    fixed missing icons

--- src/AppBundle/Resources/public/javascript/buy/components/ContentListingRightsPackage.js
+++ src/AppBundle/Resources/public/javascript/buy/components/ContentListingRightsPackage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {blueCheckIcon, yellowCheckIcon} from "../../main/components/Icons";
 
 const ContentListingRightsPackage = ({rightsPackage, programName}) => {
     return (
@@ -7,10 +8,10 @@ const ContentListingRightsPackage = ({rightsPackage, programName}) => {
                 return (
                     <div key={i} className="listing-item">
                         {!sr.exclusive &&
-                        <img style={{width: 23, height: 22, margin: '0 5px'}} src={this.blueCheck}/>}
+                        <img style={{width: 23, height: 22, margin: '0 5px'}} src={blueCheckIcon}/>}
 
                         {sr.exclusive &&
-                        <img style={{width: 23, height: 22, margin: '0 5px'}} src={this.yellowCheck}/>}
+                        <img style={{width: 23, height: 22, margin: '0 5px'}} src={yellowCheckIcon}/>}
 
                         <div style={{display: 'flex', flexDirection: "row"}}>
                             {sr.shortLabel !== "PR" && sr.name}
