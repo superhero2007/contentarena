e11b5ba2cdc40d468827c80480242b57675c22a5
Apollo <super.hero2017@outlook.com>
2018-08-12 04:57:34 -0300

    fixed migration

--- app/DoctrineMigrations/Version20180810212719.php
+++ app/DoctrineMigrations/Version20180810212719.php
@@ -18,7 +18,6 @@ class Version20180810212719 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_1C8FAF108EA2F0ED ON rights_package (short_label)');
         $this->addSql('ALTER TABLE content ADD apply_vat_in_jurisdiction TINYINT(1) NOT NULL');
     }
 
@@ -31,6 +30,5 @@ class Version20180810212719 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE content ADD distribution_packages LONGTEXT DEFAULT NULL COLLATE utf8_unicode_ci COMMENT \'(DC2Type:json_array)\', DROP apply_vat_in_jurisdiction');
-        $this->addSql('DROP INDEX UNIQ_1C8FAF108EA2F0ED ON rights_package');
     }
 }
