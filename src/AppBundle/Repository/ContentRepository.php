d1a96a3a156a63a9b250cad24766733f4c56ee56
Apollo <super.hero2017@outlook.com>
2018-09-20 00:07:21 -0300

    Show inactive listings in commercial activity

--- src/AppBundle/Repository/ContentRepository.php
+++ src/AppBundle/Repository/ContentRepository.php
@@ -395,11 +395,12 @@ class ContentRepository extends \Doctrine\ORM\EntityRepository
         return $this->createQueryBuilder('c')
             ->innerJoin("c.status", "status")
             ->andWhere('c.company = :company')
-            ->andWhere('status.name = :sold OR status.name = :approved OR status.name = :edited OR status.name = :expired' )
+            ->andWhere('status.name = :sold OR status.name = :approved OR status.name = :edited OR status.name = :expired OR status.name = :inactive' )
             ->setParameter('sold',"SOLD_OUT")
             ->setParameter('approved',"APPROVED")
             ->setParameter('edited',"EDITED")
             ->setParameter('expired',"EXPIRED")
+            ->setParameter('inactive',"INACTIVE")
             ->setParameter('company',$user->getCompany())
             ->orderBy('c.createdAt','DESC')
             ->getQuery()->getResult();
