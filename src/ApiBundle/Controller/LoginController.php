5268e310bdd6e1d06d91a809f9d7f1c9273046ca
Apollo <super.hero2017@outlook.com>
2018-09-24 23:41:53 -0300

    Login controller for ajax validation

--- src/ApiBundle/Controller/LoginController.php
+++ src/ApiBundle/Controller/LoginController.php
@@ -2,10 +2,13 @@
 
 namespace ApiBundle\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use Symfony\Component\Validator\Constraints\DateTime;
 
 class LoginController extends FOSRestController
@@ -13,11 +16,60 @@ class LoginController extends FOSRestController
     use \ApiBundle\Helper\ControllerHelper;
     use \ApiBundle\Helper\EmailHelper;
 
-    public function postLoginAction()
+    public function postLoginAction(Request $request)
     {
-        $data = array("hello" => "world");
-        $view = $this->view($data);
-        return $this->handleView($view);
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+
+        if(is_null($username) || is_null($password)) {
+            return new JsonResponse(
+                array("success" => false, "message" => 'Please verify all your inputs.'),
+                Response::HTTP_UNAUTHORIZED,
+                array('Content-type' => 'application/json')
+            );
+        }
+
+        $user_manager = $this->get('fos_user.user_manager');
+        $factory = $this->get('security.encoder_factory');
+
+        $user = $user_manager->findUserByUsername($username);
+
+        if(is_null($user)) {
+            return new JsonResponse(
+                array("success" => false, "message" => 'User not found'),
+                Response::HTTP_UNAUTHORIZED,
+                array('Content-type' => 'application/json')
+            );
+        }
+
+        $encoder = $factory->getEncoder($user);
+        $salt = $user->getSalt();
+
+        if($encoder->isPasswordValid($user->getPassword(), $password, $salt)) {
+            $response = new JsonResponse(
+                array("success" => true),
+                Response::HTTP_OK,
+                array('Content-type' => 'application/json')
+            );
+            // Here, "public" is the name of the firewall in your security.yml
+            $token = new UsernamePasswordToken($user, $user->getPassword(), "public", $user->getRoles());
+
+            // For older versions of Symfony, use security.context here
+            $this->get("security.token_storage")->setToken($token);
+
+            // Fire the login event
+            // Logging the user in above the way we do it doesn't do this automatically
+            $event = new InteractiveLoginEvent($request, $token);
+            $this->get("event_dispatcher")->dispatch("security.interactive_login", $event);
+        } else {
+            $response = new JsonResponse(
+                array("success" => false),
+                Response::HTTP_UNAUTHORIZED,
+                array('Content-type' => 'application/json')
+            );
+        }
+
+        return $response;
     }
 
     public function postRegisterAction(Request $request)
@@ -61,12 +113,12 @@ class LoginController extends FOSRestController
 
         $confirmationUrl = $this->container->get('router')->generate('fos_user_registration_confirm', array('token' => $user->getConfirmationToken()), UrlGeneratorInterface::ABSOLUTE_URL);
 
-        $this->sendEmail(
+        /*$this->sendEmail(
             'Registration/email.txt.twig',
             'Welcome to Content Arena',
             $request->get("email"),
             array('user' => $user, 'confirmationUrl' => $confirmationUrl)
-        );
+        );*/
 
         //if (!$user) {
         //    throw $this->createNotFoundException();
