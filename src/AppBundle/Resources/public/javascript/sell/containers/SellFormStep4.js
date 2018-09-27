bd9a01e7b51724c8142ff85749698245cd3118e7
Apollo <super.hero2017@outlook.com>
2018-09-27 14:34:22 +0300

    MINOR_UI_FIX_P4 - (Q - CL4 - Add Sales Bundle_Payment Schedule: DATE cannot be in the past also removed old commented code).

--- src/AppBundle/Resources/public/javascript/sell/containers/SellFormStep4.js
+++ src/AppBundle/Resources/public/javascript/sell/containers/SellFormStep4.js
@@ -163,10 +163,6 @@ class SellFormStep4 extends React.Component {
 
                     <TitleBar title={this.context.t("CL_STEP4_LISTING_DETAILS_TEXT")} infoText={this.context.t('CL_STEP4_INFO_DETAILS')}/>
 
-                    {/*<div className="step-item-description" style={{marginTop: 0}}>
-                        {this.context.t("CL_STEP4_LISTING_DETAILS_TEXT")}
-                    </div>*/}
-
                     <ListingName/>
 
                     <div className='row'>
