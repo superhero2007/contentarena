edf17d8962ac631c556c4ea442209390a18dfb48
Apollo <super.hero2017@outlook.com>
2019-03-05 20:37:22 +0200

    Lint fixes

--- src/AppBundle/Resources/public/javascript/common/components/Loader/loader.test.js
+++ src/AppBundle/Resources/public/javascript/common/components/Loader/loader.test.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import Loader from './Loader';
-import renderer from 'react-test-renderer';
-import { withContext } from './../../utils/testing';
+import React from "react";
+import renderer from "react-test-renderer";
+import Loader from "./Loader";
+import { withContext } from "../../utils/testing";
 
-it('Loader renders correctly in loading state', () => {
-    const tree = renderer
-        .create(withContext(<Loader loading>Test</Loader>))
-        .toJSON();
-    expect(tree).toMatchSnapshot();
+it("Loader renders correctly in loading state", () => {
+	const tree = renderer
+		.create(withContext(<Loader loading>Test</Loader>))
+		.toJSON();
+	expect(tree).toMatchSnapshot();
 });
 
-it('Loader renders correctly in inactive state', () => {
-    const tree = renderer
-        .create(withContext(<Loader>Test</Loader>))
-        .toJSON();
-    expect(tree).toMatchSnapshot();
+it("Loader renders correctly in inactive state", () => {
+	const tree = renderer
+		.create(withContext(<Loader>Test</Loader>))
+		.toJSON();
+	expect(tree).toMatchSnapshot();
 });
--- src/AppBundle/Resources/public/javascript/common/utils/testing/index.js
+++ src/AppBundle/Resources/public/javascript/common/utils/testing/index.js
@@ -1,7 +1,5 @@
-import getElementWithContext from 'react-test-context-provider';
+import getElementWithContext from "react-test-context-provider";
 
-const contextObject = {t: key => key};
+const contextObject = { t: key => key };
 
-export const withContext = (component) => {
-    return getElementWithContext(contextObject, component)
-};
+export const withContext = component => getElementWithContext(contextObject, component);
