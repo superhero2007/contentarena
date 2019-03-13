2686d63a23536031d23d175e55a3bf02f8b6aa49
Apollo <super.hero2017@outlook.com>
2019-03-13 03:30:15 -0300

    Webpack config for phpstorm - it makes aliases work

---
+++ phpstorm.webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports ={
+	resolve: {
+		alias : {
+			'@components': path.resolve(__dirname, 'src/AppBundle/Resources/public/javascript/common/components/'),
+			'@utils': path.resolve(__dirname, 'src/AppBundle/Resources/public/javascript/common/utils/'),
+			'@constants': path.resolve(__dirname, 'src/AppBundle/Resources/public/javascript/common/constants.js'),
+			'@icons': path.resolve(__dirname, 'src/AppBundle/Resources/public/javascript/main/components/Icons.js'),
+			'@translations': path.resolve(__dirname, 'src/AppBundle/Resources/public/javascript/translations/translations.js'),
+		}
+	}
+};
