bde37fe453f44a0e314aac54199cceb3e40dcd31
Apollo <super.hero2017@outlook.com>
2019-03-13 03:30:15 -0300

    Added header so BE can know when a request is an ajax request

--- src/AppBundle/Resources/public/javascript/common/request.js
+++ src/AppBundle/Resources/public/javascript/common/request.js
@@ -3,7 +3,10 @@ import showErrorModal from "./modals/ExpiredSessionModal/ExpiredSessionModal";
 
 const instance = axios.create({
 	baseURL: `${envhosturl}/`,
-	headers: { "Content-Type": "application/json" },
+	headers: {
+		"Content-Type": "application/json",
+		"X-Requested-With": "XMLHttpRequest",
+	},
 });
 
 instance.interceptors.response.use(response => response, (error) => {
