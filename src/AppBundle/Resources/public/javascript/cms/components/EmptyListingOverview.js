6a8c061d39842cd8700018aba796b9b68fd570f1
Apollo <super.hero2017@outlook.com>
2019-05-24 04:21:30 +0000

    Merged in listing_overview_empty_screen (pull request #364)
    changed icon on empty listing screen

--- src/AppBundle/Resources/public/javascript/cms/components/EmptyListingOverview.js
+++ src/AppBundle/Resources/public/javascript/cms/components/EmptyListingOverview.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import { ROUTE_PATHS } from "@constants";
+import { cmsFile } from "../../main/components/Icons";
 
 const EmptyListingOverview = ({ history }, context) => (
 	<div className="empty-listing">
-		<i className="fa fa-5x fa-files-o" />
+		<img src={cmsFile} alt="" />
 		<h3>{context.t("CMS_EMPTY_LISTING_OVERVIEW_TITLE")}</h3>
 		<h4>{context.t("CMS_EMPTY_LISTING_OVERVIEW_TEXT")}</h4>
 		<a className="yellow-button" href={ROUTE_PATHS.CREATE_LISTING}>
