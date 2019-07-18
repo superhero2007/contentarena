3c02e815065f1c55b87dd88675cf29a41816013b
Apollo <super.hero2017@outlook.com>
2019-07-18 02:34:24 +0000

    Merged in bug/remove-scroll (pull request #420)
    Remove scrollbar from textareas

--- src/AppBundle/Resources/public/javascript/buy/components/RightTableItem.js
+++ src/AppBundle/Resources/public/javascript/buy/components/RightTableItem.js
@@ -2,13 +2,27 @@ import React from "react";
 import { PropTypes } from "prop-types";
 import cn from "classnames";
 import Translate from "@components/Translator/Translate";
-import { RepresentationTextArea } from "../../sell/components/SellFormItems";
 
 class RightTableItem extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {};
+		this.textArea = React.createRef();
 	}
+
+	componentDidMount() {
+		this.setTextAreaHeight();
+	}
+
+	componentDidUpdate() {
+		this.setTextAreaHeight();
+	}
+
+	setTextAreaHeight = () => {
+		if (this.textArea.current) {
+			this.textArea.current.style.height = `${this.textArea.current.scrollHeight}px`;
+		}
+	};
 
 	render() {
 		const {
@@ -24,7 +38,7 @@ class RightTableItem extends React.Component {
 				<div className="right-definition-content">
 					{!textarea && !translate && value}
 					{!textarea && translate && <Translate i18nKey={value} /> }
-					{textarea && <RepresentationTextArea value={value} />}
+					{textarea && <textarea readOnly className="representation-textarea" ref={this.textArea} value={value} />}
 				</div>
 			</td>
 		);
