0f5967e2b989a466f9395b3324c7e1833baf288e
Apollo <super.hero2017@outlook.com>
2019-07-24 06:48:35 -0400

    Make a component for Territory Filter

--- src/AppBundle/Resources/public/javascript/cms/containers/CmsCommercialOverview.js
+++ src/AppBundle/Resources/public/javascript/cms/containers/CmsCommercialOverview.js
@@ -1,15 +1,13 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { PropTypes } from "prop-types";
 import Select from "react-select";
 import first from "lodash/first";
 import Translate from "@components/Translator/Translate";
 import EmptyCommercialOverview from "../components/EmptyScreens/EmptyCommercialOverview";
-import RightsLegend from "../../main/components/RightsLegend";
+// import RightsLegend from "../../main/components/RightsLegend";
 import CommercialBidsTable from "../components/CommercialBidsTable";
-import PopupCountrySelector from "../../main/components/PopupCountrySelector";
-import CountrySelector from "../../main/components/CountrySelector";
-import { cancelIcon } from "../../main/components/Icons";
+import TerritoryFilter from "../../main/components/TerritoryFilter";
 
 class CmsCommercialOverview extends React.Component {
 	constructor(props) {
@@ -130,21 +128,6 @@ class CmsCommercialOverview extends React.Component {
 			includeAllCountries,
 		} = this.state;
 
-		const countriesValue = first(countries) ? {
-			label: first(countries),
-			value: first(countries),
-		} : "";
-
-		const isWorldWideCountriesSelected = countries.length === this.worldwideCountries;
-		const isMoreThanOneSelected = countries.length > 1 && countries.length !== this.worldwideCountries;
-		const countriesInputValueObj = {
-			isShown: isWorldWideCountriesSelected || isMoreThanOneSelected,
-			value: isMoreThanOneSelected ? `${countries.length} territories`
-				: isWorldWideCountriesSelected ? "Worldwide" : "",
-			isDisabled: isMoreThanOneSelected && !isWorldWideCountriesSelected,
-			isReadonly: isWorldWideCountriesSelected,
-		};
-
 		let allListings = listings;
 		if (selectedListings) {
 			allListings = allListings.filter(list => selectedListings.value === list.customId);
@@ -194,7 +177,7 @@ class CmsCommercialOverview extends React.Component {
 			<section className="commercial-overview-tab">
 				<div className="region-filter">
 					<div className="d-flex">
-						<div className="split-filter">
+						<div className="split-filter" style={{ width: "100%" }}>
 							<div className="region-filter-title">
 								<div className="title-wrapper">
 									{ <Translate i18nKey="CMS_PROPERTY_TAB_COMMERCIAL" />}
@@ -220,56 +203,23 @@ class CmsCommercialOverview extends React.Component {
 									</div>
 								</div>
 
-								<div className="listing-filter territories-filter">
-									{countriesInputValueObj.isShown ? (
-										<Fragment>
-											<input
-												type="text"
-												className="ca-form-control"
-												value={countriesInputValueObj.value}
-												disabled={countriesInputValueObj.isDisabled}
-												readOnly={countriesInputValueObj.isReadonly}
-											/>
-
-											<img
-												className="territories-icon"
-												src={cancelIcon}
-												onClick={() => {
-													this.selectTerritory([]);
-												}}
-												alt=""
-											/>
-
-										</Fragment>
-									) : (
-										<CountrySelector
-											multi={false}
-											className="base-input-select"
-											value={countriesValue}
-											onChange={(c) => {
-												this.selectTerritory([c]);
-											}}
-										/>
-									)}
-
-									<PopupCountrySelector
-										ref="countrySelector"
-										value={countries}
-										includeAllCountries={includeAllCountries}
-										onChangeRadio={(c) => {
-											this.updateIncludedCountries(c);
-										}}
-										onSelect={this.selectTerritory}
-									/>
-								</div>
+								<TerritoryFilter
+									className="listing-filter territories-filter"
+									countries={countries}
+									includeAllCountries={includeAllCountries}
+									selectTerritory={this.selectTerritory}
+									updateIncludedCountries={this.updateIncludedCountries}
+								/>
 							</div>
 						</div>
+						{/*
 						<div className="split-filter">
 							<div className="region-filter-title">
 								{ <Translate i18nKey="CMS_RIGHT_LEGENDS_TITLE" />}
 							</div>
 							<RightsLegend isNew />
 						</div>
+						*/}
 					</div>
 					<div className="region-filter-bids">
 						<div className="region-filter-title toggle" onClick={openBidsList.length ? this.toggleOpenBids : null}>
