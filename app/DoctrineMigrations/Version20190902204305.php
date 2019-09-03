1069a56b47d9c01fb765aa715f06acd7a744ba98
Apollo <super.hero2017@outlook.com>
2019-09-03 03:46:17 -0400

    Test CI

--- app/DoctrineMigrations/Version20190902204305.php
+++ app/DoctrineMigrations/Version20190902204305.php
@@ -19,7 +19,6 @@ class Version20190902204305 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE log (id INT AUTO_INCREMENT NOT NULL, message LONGTEXT NOT NULL, context LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', level SMALLINT NOT NULL, level_name VARCHAR(50) NOT NULL, extra LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_F0E45BA9A770AC6E ON season (externalId)');
     }
 
     /**
@@ -31,6 +30,5 @@ class Version20190902204305 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE log');
-        $this->addSql('DROP INDEX UNIQ_F0E45BA9A770AC6E ON season');
     }
 }
